603756eb631f313cf98a970f7065bfe7d1e074df
Емельянов Александр Геннадьевич <A.Emelianov@gazprom-media.com>
2019-10-17 18:50:51 +0300

    1 commit

---
+++ 1.1-intro-and-branching/1.1.1/index.php
@@ -0,0 +1,41 @@
+<?php
+$variable = 3.14;
+
+if (is_bool($variable)) {
+    $type = 'bool';
+    if ($variable == false) {
+        $variable = 'false';
+    } elseif ($variable == true) {
+        $variable = 'true';
+    }
+    $description = 'Логический тип данных.<br>Используется для проверки на true или false';
+} elseif (is_float($variable)) {
+    $type = 'float';
+    $description = 'Число с плавающей точкой.<br>Используется для вещественных чисел';
+} elseif (is_int($variable)) {
+    $type = 'int';
+    $description = 'Целое число.<br>Один из простейших и самых распространённых типов данных';
+} elseif (is_string($variable)) {
+    $type = 'string';
+    $description = 'Строка.<br>Тип данных, значениями которого является произвольная последовательность символов алфавита';
+} elseif (is_null($variable)) {
+    $type = 'null';
+    $variable = (unset)$variable;
+    $description = 'Псевдозначение.<br>Может быть записано в поле таблицы базы данных';
+} elseif(is_array($variable) or is_object($variable) or is_resource($variable)) {
+    $type = 'other';
+    $description = 'Другие типы данных.<br>Такие как массив, объект, ресурс';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>bPHP - 1.1.1</title>
+</head>
+<body>
+    <p><?php echo "$variable is $type";?></p>
+    <h3><?php echo $description?></h3>
+</body>
+</html>
---
+++ 1.1-intro-and-branching/1.1.2/index.php
@@ -0,0 +1,50 @@
+<?php
+    $time = date("H");
+    $dayOfTheWeek = date("N");
+
+    if ($time >= 6 and $time <= 10) {
+        $image = 'img/morning.jpg';
+        $greeting = 'Доброе утро!';
+    } elseif ($time >= 11 and $time <= 17) {
+        $image = 'img/day.jpg';
+        $greeting = 'Добрый день!';
+    } elseif ($time >= 18 and $time <= 22) {
+        $image = 'img/evening.jpg';
+        $greeting = 'Добрый вечер!';
+    } elseif (($time >= 0 and $time <= 5) and ($time = 23)) {
+        $image = 'img/night.jpg';
+        $greeting = 'Доброй ночи!';
+    }
+
+    if ($dayOfTheWeek == 1) {
+        $day = 'Сегодня - понедельник.';
+    } elseif ($dayOfTheWeek == 2) {
+        $day = 'Сегодня - вторник.';
+    } elseif ($dayOfTheWeek == 3) {
+        $day = 'Сегодня - среда.';
+    } elseif ($dayOfTheWeek == 4) {
+        $day = 'Сегодня - четверг.';
+    } elseif ($dayOfTheWeek == 5) {
+        $day = 'Сегодня - пятница.';
+    } elseif ($dayOfTheWeek == 6) {
+        $day = 'Сегодня - суббота.';
+    } elseif ($dayOfTheWeek == 7) {
+        $day = 'Сегодня - воскресенье.';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>bPHP - 1.1.1</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="img" style="background-image: url(<?= $image; ?>)">
+        <div class="greeting">
+            <h1><?php echo "$greeting $day"; ?></h1>
+        </div>
+    </div>
+</body>
+</html>
