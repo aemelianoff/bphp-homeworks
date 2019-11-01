50c20e7d7113bfc64df7b7b7d22ac1b6a9fada3f
Емельянов Александр Геннадьевич <A.Emelianov@gazprom-media.com>
2019-11-01 13:14:52 +0300

    commit

--- 3.2-PHP7-PSR-PHPDoc/3.2.2/PoschitatSchet.php
+++ 3.2-PHP7-PSR-PHPDoc/3.2.2/PoschitatSchet.php
@@ -1,13 +1,88 @@
-<?php function PoschitatSchet($a, $b)
-{$nu = random_int(1000, 9999);$purch = "<div class=\"order322-line order322-title\">Счёт №$nu</div>";
-$syMMa = 0;foreach ($a as $i) {$znach = $b[$i->id];if ($znach > 0) {$sum = $znach * $i->price;
-$sum2 = number_format($i->price * $znach,2);$c =  number_format($i->price,2);
-$purch .= "<div class=\"order322-line\"><div>$i->name</div><div>$znach * $c ₽ = $sum2 ₽</div></div>";
-$syMMa += $sum;}}$se = (int)$b['service'];if($syMMa > 0){if($se === 2){$se1 = number_format($syMMa * 0.10,2);
-$purch .= "<div class=\"order322-line\"><div>Скидка 10% (самовывоз)</div><div>- $se1 ₽</div></div>";
-$syMMa = $syMMa - (float)$se1;} elseif ($se === 4){$se1 = number_format($syMMa * 0.10,2);
-$purch .= "<div class=\"order322-line\"><div>Чаевые 10%</div><div>$se1 ₽</div></div>";
-$syMMa = $syMMa + (float)$se1;} elseif ($se === 1){$se1 = number_format($syMMa * 0.10,2);
-$purch .= "<div class=\"order322-line\"><div>Доставка</div><div>200.00 ₽</div></div>";
-$syMMa = $syMMa + 200;}}else{$purch .= "<div class=\"order322-line\">Ничего не заказано</div>";}
-$syMMa = number_format($syMMa,2);$purch .= "<div class=\"order322-total\"><div>Итого: $syMMa ₽</div></div>";return $purch;}
+<?php
+
+const NUMBER_OF_DECIMAL_PLACES = 2;
+
+function Payment(int $amount) {
+    $send = number_format($amount * 0.10, NUMBER_OF_DECIMAL_PLACES);
+    return $send;
+}
+
+function PoschitatSchet(array $menu, array $post)
+{
+    $accountNumber = random_int(1000, 9999);
+    $purchase = "<div class=\"order322-line order322-title\">
+                    Счёт №$accountNumber
+              </div>";
+    $amount = 0;
+
+    foreach ($menu as $v) {
+        $value = $post[$v->id];
+
+        if ($value > 0) {
+            $sum = $value * $v->price;
+            $sumFormat = number_format($v->price * $value, NUMBER_OF_DECIMAL_PLACES);
+            $cost = number_format($v->price, NUMBER_OF_DECIMAL_PLACES);
+            $purchase .= "<div class=\"order322-line\">
+                            <div>
+                                $v->name
+                            </div>
+                            <div>
+                                $value * $cost ₽ = $sumFormat ₽
+                            </div>
+                        </div>";
+            $amount += $sum;
+        }
+    }
+
+    $service = (int)$post['service'];
+
+    if ($amount > 0) {
+        if ($service === 2) {
+            $send = Payment($amount);
+            $purchase .= "<div class=\"order322-line\">
+                            <div>
+                                Скидка 10% (самовывоз)
+                            </div>
+                            <div>
+                                - $send ₽
+                            </div>
+                        </div>";
+            $amount = $amount - (float)$send;
+        } elseif ($service === 4) {
+            $send = Payment($amount);
+            $purchase .= "<div class=\"order322-line\">
+                            <div>
+                                Чаевые 10%
+                            </div>
+                            <div>
+                                $send ₽
+                            </div>
+                        </div>";
+            $amount = $amount + (float)$send;
+        } elseif ($service === 1) {
+            $send = Payment($amount);
+            $purchase .= "<div class=\"order322-line\">
+                            <div>
+                                Доставка
+                            </div>
+                            <div>
+                                200.00 ₽
+                            </div>
+                        </div>";
+            $amount = $amount + 200;
+        }
+    } else {
+        $purchase .= "<div class=\"order322-line\">
+                        Ничего не заказано
+                    </div>";
+    }
+
+    $amount = number_format($amount, NUMBER_OF_DECIMAL_PLACES);
+    $purchase .= "<div class=\"order322-total\">
+                    <div>
+                        Итого: $amount ₽
+                    </div>
+                </div>";
+
+    return $purchase;
+}
